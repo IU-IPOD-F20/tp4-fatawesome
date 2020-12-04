b8f1d3e4fb5e6b38e5e4a621fdb9a21533cc6ce4
fatawesome <fatawesomeee@gmail.com>
2020-12-04 20:51:42 +0300

    fix #All: Completed all duties

--- src/main/java/BankAgencyApp.java
+++ src/main/java/BankAgencyApp.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 import application.AccesBankAgency;
 import application.BankAgencyActionContext;
 import application.BankAgencyActionList;
@@ -8,9 +6,6 @@ import application.actions.AccountsManagementAction;
 import application.actions.GetAccountAction;
 import application.actions.ListAction;
 import application.actions.OperationsAction;
-import bank.BankAgency;
-import bank.Account;
-import bank.exception.AccountException;
 
 public class BankAgencyApp {
     public static BankAgencyGenericAction getListAction(BankAgencyActionList returnTo) {
@@ -38,54 +33,4 @@ public class BankAgencyApp {
 
         actionList.execute(BankAgencyActionContext.getInstance(AccesBankAgency.getBankAgency()));
     }
-
-	public static void ownerAccounts(BankAgency ag, String ownerName) {
-		Account []  t;
-
-		t = ag.getAccountsOf(ownerName);
-		if (t.length == 0) {
-			System.out.println("no account on this name ...");
-		} else {
-			System.out.println("  " + t.length + " accounts for " + ownerName);
-			for (int i=0; i<t.length; i++)
-				t[i].print();
-		}
-	}
-
-	public static void depositOnAccount(BankAgency ag, String accountNumber, double amount) {
-		Account c;
-
-		c = ag.getAccount(accountNumber);
-		if (c==null) {
-			System.out.println("Account not existing ...");
-		} else {
-			System.out.println("Balance before deposit: "+c.balance());
-			try {
-				c.deposit(amount);
-				System.out.println("Balance after deposit: "+c.balance());
-			} catch (AccountException e) {
-				System.out.println("Deposit error, Balance unchanged: " + c.balance());
-				System.out.println(e.getMessage());
-			}
-		}
-	}
-
-	public static void withdrawFromAccount(BankAgency ag, String accountNumber, double amount) {
-		Account c;
-
-		c = ag.getAccount(accountNumber);
-		if (c==null) {
-			System.out.println("Account not existing ...");
-		} else {
-			System.out.println("Balance before withdrawal: " + c.balance());
-			try {
-				c.withdraw(amount);
-				System.out.println("Balance after withdrawal: "+c.balance());
-			} catch (AccountException e) {
-				System.out.println("Withdraw error, Balance unchanged: " + c.balance());
-				System.out.println(e.getMessage());
-			}
-		}
-
-	}
 }
